9e924cd88bcf2f8a6be972fe68dbdc617f11705a
Miftah Heriyana <miftah09heriyana@gmail.com>
2022-04-12 23:07:39 +0700

    first commit

---
+++ ifelse.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author raffi
+ */
+public class ifelse {
+    public static void main(String[] args){
+        int i=0;
+        while (i<20){
+            if (1%2!=0){
+                i++;     
+                System.out.print("CCIT"+i);   
+            }
+        }
+    }
+    
+}
